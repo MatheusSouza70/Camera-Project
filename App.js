deb40dc4df7e82bbe24afa726676394698497f68
Matheus Dutra Souza <88992046+MatheusSouza70@users.noreply.github.com>
2022-08-02 13:46:39 -0300

    Add files via upload

--- App.js
+++ App.js
@@ -1,136 +1,130 @@
-import React, { useState, useEffect, useRef } from 'react'
-import {
-  StyleSheet,
-  Text,
-  View,
-  TouchableOpacity,
-  SafeAreaView,
-  Modal,
-  Image
-} from 'react-native'
+import React, { useState, useEffect, useRef } from "react";
+import { StyleSheet, Text, SafeAreaView, TouchableOpacity, Image, View, Modal} from "react-native";
+import { Camera } from "expo-camera";
+import { FontAwesome } from "@expo/vector-icons";
+import * as Permissions from "expo-permissions"
+import * as MediaLibrary from "expo-media-library";
 
-import { Camera, CameraType } from 'expo-camera'
-
-import { FontAwesome } from '@expo/vector-icons'
 
 export default function App() {
-  const camRef = useRef(null)
-  const [hasPermission, setHasPermission] = useState(null)
-  const [type, setType] = useState(CameraType.back)
-  const [capturedPhoto, setCapturedphoto] = useState(null)
-  const[open, setOpen] = useState(false)
+  const camRef = useRef(null);
+  const [type, setType] = useState(Camera.Constants.Type.back);
+  const [hasPermission, setHaspermission] = useState(null);
+  const [capturedPhoto, setcapturedPhoto] = useState(null);
+  const [open, setOpen] = useState(false);
 
   useEffect(() => {
-    ;(async () => {
-      const { status } = await Camera.requestCameraPermissionsAsync()
-      setHasPermission(status === 'granted')
-    })()
-  }, [])
+    (async () => {
+      const { status } = await Camera.requestCameraPermissionsAsync();
+      setHaspermission(status === "granted");
+    })();
+
+    (async () => {
+      const { status } = await Permissions.askAsync(Permissions.MEDIA_LIBRARY);
+      setHaspermission(status === "granted");
+    })();
+  }, []);
 
   if (hasPermission === null) {
-    return <View />
+    return <View />;
   }
 
-  if (hasPermission === false) {
-    return <Text>No access to camera</Text>
-  }
+  if (hasPermission === false) return <Text>Acesso negado!</Text>;
 
-  async function takePicture() {
+  async function takePicture(){
     if (camRef) {
-      const data = await camRef.current.takePictureAsync()
-      setCapturedphoto(data.uri)
-      setOpen(true)
+      const data = await camRef.current.takePictureAsync();
+      setcapturedPhoto(data.uri);
+      setOpen(true);
+      console.log(data);
     }
+  }
+
+  async function savePicture() {
+    const asset = await MediaLibrary.createAssetAsync(capturedPhoto)
+      .then(() => {
+        alert("Foto salva com sucesso!");
+      })
+      .catch((error) => {
+        console.log("err", error);
+      });
   }
 
   return (
     <SafeAreaView style={styles.container}>
-      <Camera style={styles.camera} type={type} ref={camRef}>
-        <View style={styles.buttonTroca}>
+      <Camera style={{ flex: 1 }} type={type} ref={camRef}>
+        <View
+          style={{
+            flex: 1,
+            backgroundColor: "transparent",
+            flexDirection: "row",
+          }}
+        >
           <TouchableOpacity
+            style={{ position: "absolute", bottom: 20, left: 20 }}
             onPress={() => {
               setType(
-                type === CameraType.back ? CameraType.front : CameraType.back
-              )
+                type === Camera.Constants.Type.back
+                  ? Camera.Constants.Type.front
+                  : Camera.Constants.Type.back
+              );
             }}
           >
-            <FontAwesome name="exchange" size={22} color="white"></FontAwesome>
-          </TouchableOpacity>
-        </View>
-        <View style={styles.Photo}>
-          <TouchableOpacity onPress={takePicture}>
-            <FontAwesome name="camera" size={22} color="white"></FontAwesome>
+            <Text style={{ fontSize: 20, marginBottom: 13, color: "#FFF" }}>
+              Trocar
+            </Text>
           </TouchableOpacity>
         </View>
       </Camera>
-      {capturedPhoto && (
-        <Modal animationType="slide" transparent={true} visible={open}>
-          <View style={styles.contentModal}>
+      <TouchableOpacity style={styles.button} onPress={takePicture}>
+        <FontAwesome name="camera" size={23} color="#FFF" />
+      </TouchableOpacity>
 
-            <TouchableOpacity
-              style={styles.closeButton}
-              onPress={() => {
-                setOpen(false)
-              }}
-            >
-              <FontAwesome name="close" size={40} color="white"></FontAwesome>
-            </TouchableOpacity>
+      {capturedPhoto && 
+        <Modal animationType="slide" transparent={false} visible={open}>
+          <View
+            style={{
+              flex: 1,
+              justifyContent: "center",
+              alignItems: "center",
+              margin: 20,
+            }}
+          >
+            <View style={{ margin: 10, flexDirection: "row" }}>
+              <TouchableOpacity
+                style={{ margin: 10 }}
+                onPress={() => setOpen(false)}
+              >
+                <FontAwesome name="window-close" size={50} color="#FF0000" />
+              </TouchableOpacity>
 
-            <Image style={styles.imgPhoto} source={{ uri: capturedPhoto }} />
+              <TouchableOpacity style={{ margin: 10 }} onPress={savePicture}>
+                <FontAwesome name="upload" size={50} color="#121212" />
+              </TouchableOpacity>
+            </View>
+
+            <Image
+              style={{ width: "100%", height: 450, borderRadius: 20 }}
+              source={{ uri: capturedPhoto }}
+            />
           </View>
         </Modal>
-      )}
+      }
     </SafeAreaView>
-  )
+  );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    justifyContent: 'center'
-  },
-  camera: {
-    width: '100%',
-    height: '100%'
-  },
-  buttonTroca: {
-    position: 'absolute',
-    bottom: 50,
-    left: 30,
-    justifyContent: 'center',
-    alignItems: 'center',
-    backgroundColor: '#000000',
-    maargin: 20,
-    height: 50,
-    width: 50,
-    borderRadius: 50
-  },
-  Photo: {
-    position: 'absolute',
-    bottom: 50,
-    right: 30,
-    justifyContent: 'center',
-    alignItems: 'center',
-    backgroundColor: '#000000',
-    maargin: 20,
-    height: 50,
-    width: 50,
-    borderRadius: 50
-  },
-  contentModal: {
-    flex: 1,
     justifyContent: "center",
-    alignItems: "flex-end",
+  },
+  button: {
+    justifyContent: "center",
+    alignItems: "center",
+    backgroundColor: "#121212",
     margin: 20,
+    borderRadius: 10,
+    height: 50,
   },
-  closeButton: {
-    position: "absolute",
-    top: 10,
-    left: 2,
-    margin: 25,
-  },
-  imgPhoto: {
-    width: "100%",
-    height: 400,
-  }
-})
+});
